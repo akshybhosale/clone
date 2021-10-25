c595a23b035d7c90d8fce43ed2c01bf13b662d11
Caio <whoiscaio@protonmail.com>
2021-10-25 14:43:54 -0300

    Styled components theme provider added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,14 @@
+import { ThemeProvider } from "styled-components";
 import GlobalStyle from "../../assets/styles/GlobalStyle";
+import theme from "../../assets/styles/theme";
 import Header from "../Header";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Header />
-    </>
+    </ThemeProvider>
   );
 }
 
