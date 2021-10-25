72bc8445ce600149aa4f5fe49dfbf3d585bcc712
Caio <whoiscaio@protonmail.com>
2021-10-25 14:37:16 -0300

    Logo added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,15 @@
 import Container from './styles';
+import logo from '../../assets/images/logo.png';
 
 function Header() {
   return (
     <Container>
-      Header component
+      <div className='navigation'>
+        <img src={logo} alt="Netflix Logo" />
+        <nav className='desktop-nav'></nav>
+        <nav className='mobile-nav'></nav>
+      </div>
+      <div className='info'></div>
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
 
 export default styled.header`
-  
+  padding: 1rem 5rem;
+
+  img {
+    height: 5rem;
+    margin-right: 2.5rem;
+  }
+
+  .desktop-nav {
+    
+  }
+
+  .mobile-nav {
+    display: none;
+  }
 `;
