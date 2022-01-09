379f8a660125aba5ffca3ae08b043cd4c5334e3c
Caio <whoiscaio@protonmail.com>
2022-01-09 12:28:55 -0300

    feat: tmdb api call file done

--- src/tmdb.js
+++ src/tmdb.js
@@ -4,6 +4,7 @@ const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
     'api-key': '94d660a629d9ae773753ee76760166b0',
+    language: 'pt-BR',
   },
 });
 
@@ -17,27 +18,35 @@ export default {
     {
       id: 'originals',
       title: 'Originais Netflix',
-      items: [],
+      items: await request('/discover/tv?with_network=213'),
     }, {
       id: 'trending',
       title: 'Recomendados para você',
-      items: [],
+      items: await request('/trending/all/week'),
     }, {
       id: 'toprated',
       title: 'Em alta',
-      items: [],
+      items: await request('/movie/top_rated'),
     }, {
       id: 'action',
       title: 'Ação',
-      items: [],
+      items: await request('/discover/movie?with_genres=28'),
     }, {
       id: 'comedy',
       title: 'Comédia',
-      items: [],
+      items: await request('/discover/movie?with_genres=35'),
     }, {
       id: 'horror',
       title: 'Terror',
-      items: [],
+      items: await request('/discover/movie?with_genres=27'),
+    }, {
+      id: 'romance',
+      title: 'Romance',
+      items: await request('/discover/movie?with_genres=10749'),
+    }, {
+      id: 'documentary',
+      title: 'Documentários',
+      items: await request('/discover/movie?with_genres=99'),
     },
   ]),
 };
